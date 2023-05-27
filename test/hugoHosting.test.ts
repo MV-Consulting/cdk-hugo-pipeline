1ece6254df4ac3200532560ad24ae323257763aa
Manuel Vogel <mavogel@posteo.de>
2023-05-27 12:09:16 +0200

    test: add production hosting test

--- test/hugoHosting.test.ts
+++ test/hugoHosting.test.ts
@@ -6,7 +6,7 @@ import {
 import { Match, Template } from 'aws-cdk-lib/assertions';
 import { HugoHosting, HugoHostingProps } from '../src';
 
-test('Repository default name', () => {
+test('Development hosting', () => {
   const app = new App();
   const stack = new Stack(app, 'testStack', {
     env: {
@@ -23,6 +23,38 @@ test('Repository default name', () => {
     siteSubDomain: 'dev',
     domainName: 'example.com',
     buildStage: 'development',
+    zone: testZone,
+    hugoProjectPath: '../test/frontend-test',
+  };
+  // WHEN
+  new HugoHosting(stack, 'hugoHosting', testProps);
+
+  const template = Template.fromStack(stack);
+
+  // THEN
+  template.hasResource('AWS::Route53::HostedZone', {
+    Properties: Match.objectLike({
+      Name: 'example.com.',
+    }),
+  });
+});
+
+test('Production hosting', () => {
+  const app = new App();
+  const stack = new Stack(app, 'testStack', {
+    env: {
+      region: 'us-east-1',
+      account: '1234',
+    },
+  });
+
+  const testZone = new route53.HostedZone(stack, 'HostedZone', {
+    zoneName: 'example.com',
+  });
+
+  const testProps: HugoHostingProps = {
+    domainName: 'example.com',
+    buildStage: 'production',
     zone: testZone,
     hugoProjectPath: '../test/frontend-test',
   };
