21f26fd53ab291d2026945998f5452ce3ccdecc9
Manuel Vogel <mavogel@posteo.de>
2023-05-31 15:13:46 +0200

    test: enhance hosting tests

--- test/hugoHosting.test.ts
+++ test/hugoHosting.test.ts
@@ -26,7 +26,6 @@ test('Development hosting', () => {
   const template = Template.fromStack(stack);
 
   // THEN
-  // TODO test something else
   template.hasResource('AWS::S3::Bucket', {
     Properties: Match.objectLike({
       BucketName: 'dev.example.com',
@@ -39,6 +38,57 @@ test('Development hosting', () => {
     }),
     DeletionPolicy: 'Delete',
     UpdateReplacePolicy: 'Delete',
+  });
+
+  template.hasResourceProperties('AWS::CloudFront::CloudFrontOriginAccessIdentity', {
+    CloudFrontOriginAccessIdentityConfig: Match.objectLike({
+      Comment: 'OAI for hugoHosting',
+    }),
+  });
+
+  template.hasResourceProperties('AWS::CloudFront::Distribution', {
+    DistributionConfig: Match.objectLike({
+      Aliases: Match.arrayWith(['dev.example.com']),
+      CustomErrorResponses: Match.arrayWith([
+        Match.objectLike({
+          ErrorCachingMinTTL: 1800,
+          ErrorCode: 403,
+          ResponseCode: 404,
+          ResponsePagePath: '/en/404.html',
+        }),
+        Match.objectLike({
+          ErrorCachingMinTTL: 1800,
+          ErrorCode: 404,
+          ResponseCode: 404,
+          ResponsePagePath: '/en/404.html',
+        }),
+      ]),
+      DefaultRootObject: 'index.html',
+      DefaultCacheBehavior: Match.objectLike({
+        Compress: true,
+        ViewerProtocolPolicy: 'redirect-to-https',
+        FunctionAssociations: Match.arrayWith([
+          Match.objectLike({
+            EventType: 'viewer-request',
+          }),
+        ]),
+      }),
+    }),
+  });
+
+  template.hasResourceProperties('AWS::Route53::RecordSet', {
+    Name: 'dev.example.com.',
+    Type: 'A',
+    HostedZoneId: 'DUMMY',
+  });
+
+  template.hasResource('Custom::CDKBucketDeployment', {
+    Properties: Match.objectLike({
+      Prune: true,
+      DistributionPaths: Match.arrayWith(['/*']),
+    }),
+    UpdateReplacePolicy: 'Delete',
+    DeletionPolicy: 'Delete',
   });
 });
 
@@ -74,5 +124,56 @@ test('Production hosting', () => {
     }),
     DeletionPolicy: 'Retain',
     UpdateReplacePolicy: 'Retain',
+  });
+
+  template.hasResourceProperties('AWS::CloudFront::CloudFrontOriginAccessIdentity', {
+    CloudFrontOriginAccessIdentityConfig: Match.objectLike({
+      Comment: 'OAI for hugoHosting',
+    }),
+  });
+
+  template.hasResourceProperties('AWS::CloudFront::Distribution', {
+    DistributionConfig: Match.objectLike({
+      Aliases: Match.arrayWith(['example.com']),
+      CustomErrorResponses: Match.arrayWith([
+        Match.objectLike({
+          ErrorCachingMinTTL: 1800,
+          ErrorCode: 403,
+          ResponseCode: 404,
+          ResponsePagePath: '/en/404.html',
+        }),
+        Match.objectLike({
+          ErrorCachingMinTTL: 1800,
+          ErrorCode: 404,
+          ResponseCode: 404,
+          ResponsePagePath: '/en/404.html',
+        }),
+      ]),
+      DefaultRootObject: 'index.html',
+      DefaultCacheBehavior: Match.objectLike({
+        Compress: true,
+        ViewerProtocolPolicy: 'redirect-to-https',
+        FunctionAssociations: Match.arrayWith([
+          Match.objectLike({
+            EventType: 'viewer-request',
+          }),
+        ]),
+      }),
+    }),
+  });
+
+  template.hasResourceProperties('AWS::Route53::RecordSet', {
+    Name: 'example.com.',
+    Type: 'A',
+    HostedZoneId: 'DUMMY',
+  });
+
+  template.hasResource('Custom::CDKBucketDeployment', {
+    Properties: Match.objectLike({
+      Prune: true,
+      DistributionPaths: Match.arrayWith(['/*']),
+    }),
+    UpdateReplacePolicy: 'Delete',
+    DeletionPolicy: 'Delete',
   });
 });
